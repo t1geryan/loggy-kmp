bfbf2d6fd8ba14403c428cb1913960c5fdcc85f0
t1geryan <sgta1980@gmail.com>
2025-03-09 23:41:58 +0300

    Refactored Level.kt as sealed class

--- loggy/src/commonMain/kotlin/com/tigeryan/loggy/Level.kt
+++ loggy/src/commonMain/kotlin/com/tigeryan/loggy/Level.kt
@@ -1,10 +1,20 @@
 package com.tigeryan.loggy
 
-public enum class Level {
-    VERBOSE,
-    DEBUG,
-    INFO,
-    WARNING,
-    ERROR,
-    FATAL
+public sealed class Level(private val value: Int) : Comparable<Level> {
+
+    override fun compareTo(other: Level): Int {
+        return this.value.compareTo(other.value)
+    }
+
+    public data object Verbose : Level(0)
+
+    public data object Debug : Level(1)
+
+    public data object Info : Level(2)
+
+    public data object Warning : Level(3)
+
+    public data object Error : Level(4)
+
+    public data object Fatal : Level(5)
 }
